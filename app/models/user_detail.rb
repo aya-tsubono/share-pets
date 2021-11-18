55d84e36b8a0b87fcc910cb8636114c5ac9709a8
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-19 00:28:58 +0900

    ActiveHashのバリデーションとアソシエーションの記述

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,4 +2,20 @@ class UserDetail < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :birthday, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :household
+  belongs_to :pet_friendly
+  belongs_to :have_surgery
+  belongs_to :get_vaccine
+  belongs_to :visit
+  belongs_to :consent
+
+  validates :household_id,    numericality: { other_than: 1, message: "can't be blank" }
+  validates :pet_friendly_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :have_surgery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :get_vaccine_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :visit_id,        numericality: { other_than: 1, message: "can't be blank" }
+  validates :consent_id,      numericality: { other_than: 1, message: "can't be blank" }
+
 end
