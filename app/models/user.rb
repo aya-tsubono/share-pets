86cdae2833f0daf1023d4b1e4ecbde0cff863f52
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-18 23:41:45 +0900

    prefectureモデルの作成とバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   validates :password,                       format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
   validates :full_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :full_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :prefecture_id,  presence: true
   validates :phone_number,   presence: true, format: { with: /\A[0-9]{10,11}\z/ }
        
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+
 end
