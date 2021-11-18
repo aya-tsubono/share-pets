1296b1813e8577018562f01429f9f458052ef7cb
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-19 01:00:20 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'user_details', to: 'users/registrations#new_user_detail'
+    post 'user_details', to: 'users/registrations#create_user_detail'
+  end
   get 'pets/index'
   root to: 'pets#index'
 end
