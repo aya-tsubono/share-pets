d698425a9ceaa234f793d72ada15d08ff35c3743
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-18 23:56:32 +0900

    user_detailテーブルの作成

--- db/migrate/20211118145333_create_user_details.rb
+++ db/migrate/20211118145333_create_user_details.rb
@@ -1,7 +1,21 @@
 class CreateUserDetails < ActiveRecord::Migration[6.0]
   def change
     create_table :user_details do |t|
-
+      t.date    :bithday, null: false
+      t.integer :household_id
+      t.string  :experience
+      t.string  :have_animal
+      t.integer :pet_friendly_id
+      t.text    :desired_animal
+      t.string  :economic_status
+      t.string  :deposit
+      t.integer :have_surgery_id
+      t.integer :get_vaccine_id
+      t.string  :indoor_outdoor
+      t.integer :visit_id
+      t.integer :consent_id
+      t.text    :remarks
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_135728) do
+ActiveRecord::Schema.define(version: 2021_11_18_145333) do
+
+  create_table "user_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "bithday", null: false
+    t.integer "household_id"
+    t.string "experience"
+    t.string "have_animal"
+    t.integer "pet_friendly_id"
+    t.text "desired_animal"
+    t.string "economic_status"
+    t.string "deposit"
+    t.integer "have_surgery_id"
+    t.integer "get_vaccine_id"
+    t.string "indoor_outdoor"
+    t.integer "visit_id"
+    t.integer "consent_id"
+    t.text "remarks"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_details_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +50,5 @@ ActiveRecord::Schema.define(version: 2021_11_18_135728) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_details", "users"
 end
