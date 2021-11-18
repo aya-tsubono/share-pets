09bbd0c750838a0345b7f5a06a458044e878aa3f
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-18 23:10:54 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,       presence: true
+  validates :password,                       format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+  validates :full_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :full_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :prefecture_id,  presence: true
+  validates :phone_number,   presence: true, format: { with: /\A[0-9]{10,11}\z/ }
+       
 end
