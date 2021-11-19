018e11e83dc4cd6bc1f1353227c02b7d9c9b9b86
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-19 16:02:27 +0900

    詳細登録ビューの修正

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,8 +1,8 @@
-class Vsit < ActiveHash::Base
+class Visit < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '譲渡者の自宅訪問実施可能' },
-    { id: 3, name: '譲渡者の自宅訪問実施不可' },
+    { id: 2, name: '自宅訪問実施可能' },
+    { id: 3, name: '自宅訪問実施不可' },
   ]
 
   include ActiveHash::Associations
