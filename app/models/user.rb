9a70b7c5ef437fc1f16de5ecd6e6ea72a2ca1dab
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-21 00:07:43 +0900

    userモデルのテストの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   
   validates :nickname,       presence: true
   validates :password,                       format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
-  validates :full_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :full_name,      presence: true
   validates :full_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :phone_number,   presence: true, format: { with: /\A[0-9]{10,11}\z/ }
        
