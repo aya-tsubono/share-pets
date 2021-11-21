b8b7beefffaecb3fbf8af92ef58c4f170e2452cf
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-21 23:46:10 +0900

    user_detailのテストの記述

--- spec/models/user_detail_spec.rb
+++ spec/models/user_detail_spec.rb
@@ -10,35 +10,64 @@ RSpec.describe UserDetail, type: :model do
       it '全ての項目を入力すれば登録できる' do
         expect(@user_detail).to be_valid
       end
-      it "household_idが空でも登録できる" do
+      it "household_idを選択しなくても登録できる" do
+        @user_detail.household_id = 1
+        expect(@user_detail).to be_valid
       end
       it "experienceが空でも登録できる" do
+        @user_detail.experience = ''
+        expect(@user_detail).to be_valid
       end
       it "have_animalが空でも登録できる" do
+        @user_detail.have_animal = ''
+        expect(@user_detail).to be_valid
       end
-      it "pet_friendly_idが空でも登録できる" do
+      it "pet_friendly_idが選択しなくても登録できる" do
+        @user_detail.pet_friendly_id = 1
+        expect(@user_detail).to be_valid
       end
       it "desired_animalが空でも登録できる" do
+        @user_detail.desired_animal = ''
+        expect(@user_detail).to be_valid
       end
       it "economic_statusが空でも登録できる" do
+        @user_detail.economic_status = ''
+        expect(@user_detail).to be_valid
       end
       it "depositが空でも登録できる" do
+        @user_detail.deposit = ''
+        expect(@user_detail).to be_valid
       end
-      it "have_surgery_idが空でも登録できる" do
+      it "have_surgery_idが選択しなくても登録できる" do
+        @user_detail.have_surgery_id = 1
+        expect(@user_detail).to be_valid
       end
-      it "get_vaccine_idが空でも登録できる" do
+      it "get_vaccine_idが選択しなくても登録できる" do
+        @user_detail.get_vaccine_id = 1
+        expect(@user_detail).to be_valid
       end
       it "indoor_outdoorが空でも登録できる" do
+        @user_detail.indoor_outdoor = ''
+        expect(@user_detail).to be_valid
       end
-      it "visit_idが空でも登録できる" do
+      it "visit_idが選択しなくても登録できる" do
+        @user_detail.visit_id = 1
+        expect(@user_detail).to be_valid
       end
-      it "consent_idが空でも登録できる" do
+      it "consent_idが選択しなくても登録できる" do
+        @user_detail.consent_id = 1
+        expect(@user_detail).to be_valid
       end
       it "remarksが空でも登録できる" do
+        @user_detail.remarks = ''
+        expect(@user_detail).to be_valid
       end
     end
     context '登録できない場合' do
       it "birthdayが空では登録できない" do
+        @user_detail.birthday = ''
+        @user_detail.valid?
+        expect(@user_detail.errors.full_messages).to include("Birthday can't be blank")
       end
 
     end
