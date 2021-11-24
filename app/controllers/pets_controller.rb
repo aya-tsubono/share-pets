4d144c9782c11fb3f4b59fa083c6fbba720a7467
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-24 13:09:35 +0900

    newアクションの定義とルーティングの設定

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,10 @@
 class PetsController < ApplicationController
+
   def index
   end
+
+  def new
+    @pet = Pet.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     get 'user_details', to: 'users/registrations#new_user_detail'
     post 'user_details', to: 'users/registrations#create_user_detail'
   end
+
   get 'pets/index'
   root to: 'pets#index'
+
+  resources :pets, only: [:new]
 end
