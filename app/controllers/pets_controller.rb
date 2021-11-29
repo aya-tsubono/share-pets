4357ce5acdd6e511b41d1bc61288134cbd8e6677
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-30 00:39:06 +0900

    createアクションの定義

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,7 +8,19 @@ class PetsController < ApplicationController
   end
 
   def create
-
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+
+private
+
+  def pet_params
+    params.require(:pet).permit(:title, :animal_name, :animal_category_id, :breed, :sex_id, :age_id, :weight_id, :status, :prefecture_id, :vaccination_id, :castration_id,
+      :area, :reason, :charge, :condition, :image).merge(user_id: current_user.id)
+  end
 end
