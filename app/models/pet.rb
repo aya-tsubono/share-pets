4daa0b0006dd6112d240c2898ddce3e08a7eecd0
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-30 14:35:43 +0900

    petモデルにActiveHashのアソシエーションとバリデーションの記述

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -11,4 +11,21 @@ class Pet < ApplicationRecord
   validates :charge,      presence: true,                           inclusion: { in: 0..9_999_999 }, format: { with: /\A[0-9]+\z/ }, numericality: true
   validates :condition,   presence: true, length: { maximum: 500 }
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :animal_category
+  belongs_to :sex
+  belongs_to :age
+  belongs_to :weight
+  belongs_to :prefecture
+  belongs_to :vaccination
+  belongs_to :castration
+
+  validates :animal_category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sex_id,             presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :age_id,             presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :weight_id,          presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :vaccination_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :castration_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+
 end
