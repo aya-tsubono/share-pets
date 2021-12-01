9772c1cac661a180a0f20e91441a272f7acd3ddf
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-01 17:48:26 +0900

    テストコードの準備

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :pet do
-    
+    title  {  }
+    animal_name  {  }
+    animal_category_id  {  }
+    breed  {  }
+    sex_id  {  }
+    age_id  {  }
+    weight_id  {  }
+    prefecture_id  {  }
+    vaccination_id  {  }
+    castration_id  {  }
+    area  {  }
+    charge  {  }
+    condition  {  }
+    reason  {  }
+    status  {  }
+    association :user
+
+    after(:build) do |pet|
+      pet.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
-
 RSpec.describe Pet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @pet = FactoryBot.build(:pet)
+  end
+
+  describe '新規投稿' do
+    context '投稿できる場合' do
+      it '全ての項目を正しく入力すれば登録できる' do
+        expect(@pet).to be_valid
+      end
+    end
+    context '登録できない場合' do
+      it 'titleが空では登録できない' do
+        @pet.title = ''
+        @pet.valid?
+        expect(@pet.errors.full_messages).to include("Title can't be blank")
+      end
+    end
+  end
 end
