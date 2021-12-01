83603ea259d71a83acfe62c9f8c71f9680a77312
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-01 19:31:18 +0900

    バリデーションの修正

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,15 +2,15 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :title,       presence: true, length: { maximum: 40 }
-  validates :animal_name, presence: true, length: { maximum: 20 }
+  validates :title,       presence: true, length: { maximum: 50 }
+  validates :animal_name, presence: true, length: { maximum: 50 }
   validates :breed,       presence: true, length: { maximum: 50 }
   validates :status,      presence: true, length: { maximum: 1000 }
   validates :area,        presence: true, length: { maximum: 200 }
   validates :reason,      presence: true, length: { maximum: 500 }
-  validates :status,      presence: true, length: { maximum: 1000 }
-  validates :charge,      presence: true
+  validates :charge,      presence: true, inclusion: { in: 0..9_999_999 }, format: { with: /\A[0-9]+\z/ }, numericality: true
   validates :condition,   presence: true, length: { maximum: 500 }
+  validates :image,       presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :animal_category
