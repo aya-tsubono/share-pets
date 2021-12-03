69b9e88a80a5686dabfffc7875b3abbd78d35d31
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-03 19:49:45 +0900

    コントローラーのコードの整理

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,7 @@
 class PetsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_pet, only: [:show, :edit, :update]
+  
   def index
     @pets = Pet.all.order('created_at DESC')
   end
@@ -19,15 +20,12 @@ class PetsController < ApplicationController
   end
 
   def show
-    @pet = Pet.find(params[:id])
   end
 
   def edit
-    @pet = Pet.find(params[:id])
   end
   
   def update
-    @pet = Pet.find(params[:id])
     if @pet.update(pet_params)
       redirect_to pet_path      
     else
@@ -42,4 +40,9 @@ class PetsController < ApplicationController
     params.require(:pet).permit(:title, :animal_name, :animal_category_id, :breed, :sex_id, :age_id, :weight_id, :status, :prefecture_id, :vaccination_id, :castration_id,
                                 :area, :reason, :charge, :condition, :image).merge(user_id: current_user.id)
   end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
 end
