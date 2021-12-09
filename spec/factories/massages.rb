ad24dfe39312c283196e81f84aa36932e4830323
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-09 14:58:16 +0900

    messageモデルのFactoryBotの記述

--- spec/factories/massages.rb
+++ spec/factories/massages.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :massage do
-    
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('assets/images/sample.jpg'), filename: 'sample.jpg')
+    end
+
   end
 end
