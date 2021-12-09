75beb0771a52ac3aa6457e7cec73b5a82fe2b79a
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-09 14:26:59 +0900

    roomモデルのバリデーションの追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,5 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users, validate: false
 
-  validates :room_name, presence: true
+  validates :room_name, presence: true, length: { maximum: 40 }
 end
