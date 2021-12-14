a64b8942f03088b4a16fcf4a2df02a25860e7cd5
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-15 02:31:09 +0900

    ユーザー詳細ページにルーム一覧の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_detail = UserDetail.find(params[:id])
     @pets = @user.pets
+    @room = @user.rooms
   end
 end
