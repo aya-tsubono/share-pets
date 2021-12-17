094428642a2ff18128c4c8dddbe1c5f2527b8283
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-17 20:08:25 +0900

    messageコントローラーの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
-    @pet = Pet.find(params[:pet_id])
+    @pet = @room.pet
   end
 
   def create
