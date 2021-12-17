c9acde0d3aeec7a9143fc200efe7b88bb36f929d
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-17 20:07:43 +0900

    スペルの修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users, validate: false
-  has_many :massages
+  has_many :messages
   belongs_to :pet, optional: true
 
   validates :room_name, presence: true
