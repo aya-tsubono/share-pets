829ff49d2b9043ab3b6f631490203eee6d3debbc
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-17 17:45:30 +0900

    roomモデルのアソシエーションとバリデーションの修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,9 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users, validate: false
+  has_many :massages
+  belongs_to :pet, optional: true
 
-  validates :room_name, presence: true, length: { maximum: 40 }
+  validates :room_name, presence: true
   validates :pet_id, presence: true
 end
