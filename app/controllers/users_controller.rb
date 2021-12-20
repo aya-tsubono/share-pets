50ba9e9a80285979326dfe70f0310bd8011363f2
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-20 19:54:28 +0900

    アクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
     @user_detail = UserDetail.find(params[:id])
     @pets = @user.pets
     @room = @user.rooms
+  end
+
+  def edit
+
+  end
+
+  def update
+
   end
 end
