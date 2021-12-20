097560bd20988e00553dcb971905c7b6217a2671
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-20 20:49:07 +0900

    updateアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     @user_detail = UserDetail.find(params[:id])
   end
 
   def update
-
+    user_detail = UserDetail.find(params[:id])
+    if user_detail.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
+
+private
+
+def user_params
+  params.require(:user_detail).permit(:birthday, :household_id, :experience, :have_animal, :pet_friendly_id, :desired_animal,
+    :economic_status, :deposit, :have_surgery_id, :get_vaccine_id, :indoor_outdoor, :visit_id, :consent_id, :remarks).merge(user_id: current_user.id)
+end
 
 end
