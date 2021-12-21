300b426a2b5b6590e564b86bad090851e4b20f43
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-22 01:03:40 +0900

    usersコントローラーの整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit]
+  before_action :set_detail, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
-    @user_detail = UserDetail.find(params[:id])
     @pets = @user.pets
     @room = @user.rooms
   end
 
   def edit
-    @user = User.find(params[:id])
-    @user_detail = UserDetail.find(params[:id])
     redirect_to root_path if @user.id != current_user.id
   end
 
   def update
-    user_detail = UserDetail.find(params[:id])
-    if user_detail.update(user_params)
+    if @user_detail.update(user_params)
       redirect_to user_path
     else
       render :edit
@@ -28,5 +25,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user_detail).permit(:birthday, :household_id, :experience, :have_animal, :pet_friendly_id, :desired_animal,
                                         :economic_status, :deposit, :have_surgery_id, :get_vaccine_id, :indoor_outdoor, :visit_id, :consent_id, :remarks).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_detail
+    @user_detail = UserDetail.find(params[:id])
   end
 end
