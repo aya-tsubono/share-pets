56f91511b94c671b59db408640415c305e2de884
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-22 00:22:55 +0900

    petのActiveHash内容の変更

--- app/models/vaccination.rb
+++ app/models/vaccination.rb
@@ -1,9 +1,8 @@
 class Vaccination < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '接種済み' },
-    { id: 3, name: '未接種' },
-    { id: 4, name: '譲渡までに接種予定' }
+    { id: 1, name: '接種済み' },
+    { id: 2, name: '未接種' },
+    { id: 3, name: '譲渡までに接種予定' }
   ]
 
   include ActiveHash::Associations
