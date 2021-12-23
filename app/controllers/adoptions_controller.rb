0d0a6d68c2ee6b0201f292198d792e6398fe9c21
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-24 00:11:51 +0900

    newアクションの定義

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -2,7 +2,13 @@ class AdoptionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def new
-
+    @pet = Pet.find(params[:pet_id])
+    if current_user.id == @pet.user_id
+      redirect_to root_path
+    elsif (current_user.id != @pet.user_id) && @pet.adoption.present?
+      redirect_to root_path
+    end
+    @adoption = Adoption.new
   end
 
   def create
