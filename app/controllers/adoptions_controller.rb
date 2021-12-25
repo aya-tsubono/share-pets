e8837dfabed7a9d46b1d208433f0b825a3ac1ab7
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-25 17:36:14 +0900

    adoptionコントローラーの修正

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -3,9 +3,11 @@ class AdoptionsController < ApplicationController
   
   def new
     @pet = Pet.find(params[:pet_id])
-    if current_user.id == @pet.user_id
+    @room = Room.find(params[:room_id])
+    @user = @room.room_users.where.not(user_id: @pet.user_id)
+    if current_user.id != @pet.user_id
       redirect_to root_path
-    elsif (current_user.id != @pet.user_id) && @pet.adoption.present?
+    elsif (current_user.id == @pet.user_id) && @pet.adoption.present?
       redirect_to root_path
     end
     @adoption = Adoption.new
@@ -21,8 +23,8 @@ class AdoptionsController < ApplicationController
   end
 
   private
-  
+
   def adoption_params
-    params.require(:adoption).merge(user_id: current_user.id, pet_id: params[:pet_id])
+    params.require(:adoption).permit(:user_id, :pet_id)
   end
 end
