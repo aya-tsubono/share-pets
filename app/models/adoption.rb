a5545fc36dcf97fa5066d80646eb2d17d18c2c02
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-12-27 18:58:04 +0900

    アソシエーションの追加

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,6 +1,6 @@
 class Adoption < ApplicationRecord
   belongs_to :pet
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :pet_id, presence: true
   validates :user_id, presence: true
