f332ec53a5f634ad560f00c9fa9ad66ed3096a40
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-01-04 00:41:22 +0900

    コードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,10 @@ class User < ApplicationRecord
   has_many :pets
   has_many :adoption
 
-  validates :nickname,       presence: true
-  validates :password,                       format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+  validates :nickname, presence: true
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
   validates :full_name,      presence: true
   validates :full_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :phone_number,   presence: true, format: { with: /\A[0-9]{10,11}\z/ }
   validates :birthday,       presence: true
-
 end
