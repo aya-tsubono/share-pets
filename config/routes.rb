4f662ea70fd573534d6ae9d5adb371a53515789e
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-01-21 17:46:08 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: [:show, :edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
